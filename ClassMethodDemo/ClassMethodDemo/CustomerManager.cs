d749afa90b75b91dfc66685729bbefa2aa869637
nrtkn <38586514+nrtkn@users.noreply.github.com>
2021-04-28 14:39:08 +0300

    Add files via upload

---
+++ ClassMethodDemo/ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name+" "+customer.Surname+" Added!");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + "'s informations updated!");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " Deleted!");
+        }
+    }
+}
---
+++ ClassMethodDemo/ClassMethodDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(new Customer
+            {
+                 Id = 17,
+                 Name = "Nuri",
+                 Surname = "Urtekin",
+            });
+            customerManager.Update(new Customer
+            {
+                Id = 17,
+                Name = "Nuri",
+                Surname = "Urtekin",
+            });
+            customerManager.Delete(new Customer
+            {
+                Id = 17,
+                Name = "Nuri",
+                Surname = "Urtekin",
+            });
+
+
+            Console.ReadLine();
+        }
+    }
+}
